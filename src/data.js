30a1a0399943fa137e8a0a8d848147da3054d374
Sudhanshu Rai <sudhanshurai025@gmail.com>
2021-12-01 14:32:02 +0530

    Added More Songs

--- src/data.js
+++ src/data.js
@@ -19,6 +19,51 @@ const songList = () =>{
             id: uuidv4(),
             active: false,
             color: ['#2A2945','#ECCD91']
+        },
+        {
+            name: "In The Name Of Love",
+            cover: "https://firebasestorage.googleapis.com/v0/b/tunite-79921.appspot.com/o/ab67616d0000b2738c77bcf5f5a227d270d23370.jfif?alt=media&token=e350425a-1ff0-46fc-b09c-3fd20c0b4ec0",
+            artist: "Martin Garix, Babe Rexha",
+            audio: "https://firebasestorage.googleapis.com/v0/b/tunite-79921.appspot.com/o/Martin-Garrix-Bebe-Rexha-In-The-Name-of-Love-(englishsongs.wapkiz.com).mp3?alt=media&token=cca7b605-3168-43a3-b538-cd59c15fcc02",
+            id: uuidv4(),
+            active: false,
+            color: ['#2A2945','#ECCD91']
+        },
+        {
+            name: "DJ Got Us Fallin' In Love",
+            cover: "https://firebasestorage.googleapis.com/v0/b/tunite-79921.appspot.com/o/ab67616d0000b27386b0c9728ad3ed338eaeea79.jfif?alt=media&token=56d713b7-57df-4dce-9d10-99cd30d9114f",
+            artist: "Usher, Pitbull",
+            audio: "https://firebasestorage.googleapis.com/v0/b/tunite-79921.appspot.com/o/usher-dj-got-us-falling-in-love.mp3?alt=media&token=b6fbb08b-d3da-423f-9716-4d7f71c4e5bd",
+            id: uuidv4(),
+            active: false,
+            color: ['#2A2945','#ECCD91']
+        },
+        {
+            name: "Who Do You Love",
+            cover: "https://firebasestorage.googleapis.com/v0/b/tunite-79921.appspot.com/o/The_Chainsmokers_-_Who_Do_You_Love.png?alt=media&token=891d7471-e2fe-4c9a-9024-7b1daad22c20",
+            artist: "Chainsmokers, 5 seconds of summer",
+            audio: "https://firebasestorage.googleapis.com/v0/b/tunite-79921.appspot.com/o/The-Chainsmokers-Who-Do-You-Love-Official-Video-ft.-5-Seconds-of-Summer.mp3?alt=media&token=e64098fe-061f-43fc-a057-f5443ea4477d",
+            id: uuidv4(),
+            active: false,
+            color: ['#2A2945','#ECCD91']
+        },
+        {
+            name: "Tigini",
+            cover: "https://firebasestorage.googleapis.com/v0/b/tunite-79921.appspot.com/o/crop_480x480_4305455.jpg?alt=media&token=ec5255b3-bb07-4357-9cb8-0ec07be3b8b0",
+            artist: "Kikimotelaba",
+            audio: "https://firebasestorage.googleapis.com/v0/b/tunite-79921.appspot.com/o/Tigini(PagalWorld).mp3?alt=media&token=827c178f-29ac-4e2e-8aba-1ec7a11eecc2",
+            id: uuidv4(),
+            active: false,
+            color: ['#2A2945','#ECCD91']
+        },
+        {
+            name: "You Broke Me First",
+            cover: "https://firebasestorage.googleapis.com/v0/b/tunite-79921.appspot.com/o/crop_480x480_3121608.jpg?alt=media&token=43eb9e74-98db-4b59-b5f1-468059e91932",
+            artist: "Tate McRae",
+            audio: "https://firebasestorage.googleapis.com/v0/b/tunite-79921.appspot.com/o/Tate-McRae-you-broke-me-first-Official-Video.mp3?alt=media&token=e0b4edc1-4d94-43ee-8ee8-405933e77caa",
+            id: uuidv4(),
+            active: false,
+            color: ['#2A2945','#ECCD91']
         }
     ]
 }
