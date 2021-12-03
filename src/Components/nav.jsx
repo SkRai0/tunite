aa776f11ff447b369988afff2ed07b05a0459445
Sudhanshu Rai <sudhanshurai025@gmail.com>
2021-12-03 13:09:41 +0530

    Added Logo

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMusic } from "@fortawesome/free-solid-svg-icons";
+import logo from './Tunite-Logo.png';
 
 const NavBar = ({libVis, setLibVis}) =>{
     return(
         <div className="nav">
-            <h2>Tunite</h2>
+            <div className="logo-brand">
+                <img className="logo" src={logo} alt="" srcset="" />
+                <h2>Tunite</h2>
+            </div>
             <button onClick={() => setLibVis(!libVis)}>
                 Library
                 <FontAwesomeIcon icon={faMusic} />
